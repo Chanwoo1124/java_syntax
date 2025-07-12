9a2183ad5e06762b0ca46066526252ba5db0d291
Chanwoo Lee <lgw22774@gmail.com>
2025-07-12 23:09:04 +0900

    java

---
+++ src/proprammers/문자열반복해서출력하기.java
@@ -0,0 +1,14 @@
+package proprammers;
+import java.util.Scanner;
+public class 문자열반복해서출력하기 {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int n = scanner.nextInt();
+        System.out.println(s.repeat(n));
+
+        for (int i =0; i<n;i++){
+            System.out.print(s);
+        }
+    }
+}
