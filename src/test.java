b8be4d1b76d46c9bd8b17751550e698550e138ff
Chanwoo Lee <lgw22774@gmail.com>
2025-07-12 21:08:16 +0900

    java printf

--- src/test.java
+++ src/test.java
@@ -1,6 +1,25 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        String url = "www.google.com";
+        //printf 는 변수의 값을 여러가지 형식으로 변환하여 출력하는 기능을 가지고 있다.
+//        int age = 14;
+//
+//        System.out.printf("age:%d%n",age);
+//        System.out.printf("age:%d",14);
+//        System.out.println("");
+//        System.out.printf("age:14%n");
+
+        System.out.printf("%s",url);
+        System.out.printf("%20s",url);
+        System.out.printf("%-20s",url);
+        System.out.printf("%.8s",url);
+
+
+
+//        float f1 = 0.10f;
+//        System.out.println(f1);
+
 
     }
 
