3d177e4b2e9659fe7d77604edbf76940318728c4
Chanwoo Lee <lgw22774@gmail.com>
2025-07-12 22:11:57 +0900

    type

--- src/input.java
+++ src/input.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
+
+
 public class input {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
+        int num = Integer.parseInt(input);
+        num += 1;
+        System.out.println(num);
+        int num2 = scanner.nextInt();
+        System.out.println(num2);
+
+
+    }
 }
