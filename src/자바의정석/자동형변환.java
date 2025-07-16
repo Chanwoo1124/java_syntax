1b48c2f88c2e91719fd6c76c01a53487451d848f
Chanwoo Lee <lgw22774@gmail.com>
2025-07-16 23:39:00 +0900

    7.16java

---
+++ src/자바의정석/자동형변환.java
@@ -0,0 +1,52 @@
+package 자바의정석;
+import java.util.Scanner;
+
+
+public class 자동형변환 {
+    public static void main(String args[]){
+//        float f = 1234;
+//        System.out.println(f);
+//        int x = 10;
+//        int y = 4;
+//        float w =(float)y;
+//        System.out.println(x/w);
+//
+//        int[] num = {1,2,3,4,5};
+//        for (int n : num){
+//            System.out.println(n);
+//
+//          String str1 ="abc";
+//          String str2 = "abc";
+//
+//          System.out.println(str1 == str2);
+//
+//          char ch = ' ';
+//          Scanner scanner = new Scanner(System.in);
+//          String s = scanner.nextLine();
+//          ch = s.charAt(0);
+//          System.out.println(ch);
+//
+           // 조건 연산자
+//          int x = 2;
+//          int y = 3;
+//
+//          int result =(x>y)? x:y;
+//          System.out.println(result);
+//
+//
+//        int i = 5;
+//        i = i & 3;
+//        System.out.println(i);
+
+          char ch = 'A';
+          int i = ch;
+          System.out.println(i);
+
+
+
+
+
+        }
+
+
+    }
