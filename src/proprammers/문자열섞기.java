97023b7bfd38703c6e6fac060aeb43b4d3e6fcee
Chanwoo1124 <lgw22774@gmail.com>
2025-08-13 21:44:54 +0900

    문자열섞기

---
+++ src/proprammers/문자열섞기.java
@@ -0,0 +1,24 @@
+package proprammers;
+import java.util.Scanner;
+
+class Solution2 {
+    public String solution(String str1, String str2){
+        String answer = "";
+        for(int i = 0; i <str1.length();i++){
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
+        }
+    return answer;
+    }
+}
+
+
+public class 문자열섞기 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Solution2 so = new Solution2();
+        String a =so.solution("aaaaa","bbbbb");
+        System.out.println(a);
+
+    }
+}
