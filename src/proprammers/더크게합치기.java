49503af5235c7802ebe7c98f73a2ad27f7fbdb0b
Chanwoo1124 <lgw22774@gmail.com>
2025-08-15 21:19:13 +0900

    더크게합치기

---
+++ src/proprammers/더크게합치기.java
@@ -0,0 +1,29 @@
+package proprammers;
+class Solution {
+    public int solution(int a, int b) {
+        String strA = String.valueOf(a);
+        String strB = String.valueOf(b);
+        int result1 = Integer.parseInt(strA+strB);
+        int result2 = Integer.parseInt(strB+strA);
+        int answer = 0;
+        if (result1>result2){
+            answer = result1;
+            System.out.println(result1);
+        } else if (result2>result1) {
+            answer = result2;
+            System.out.println(result2);
+        }else {
+            answer = result1;
+            System.out.println(result1);
+        }
+
+        return answer;
+    }
+}
+public class 더크게합치기 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution(9,91);
+
+    }
+}
