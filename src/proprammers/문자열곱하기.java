caa871b2feec4508e67f6d8c2824ae9c31c9e39c
Chanwoo1124 <lgw22774@gmail.com>
2025-08-15 21:03:57 +0900

    문자열곱하기

---
+++ src/proprammers/문자열곱하기.java
@@ -0,0 +1,13 @@
+package proprammers;
+
+public class 문자열곱하기 {
+    public static void main(String[] args) {
+        String answer = "";
+        String my_string = "asd";
+        int k = 3;
+        for (int i = 0; i < k;i++){
+            answer+= my_string;
+        }
+        System.out.println(answer);
+    }
+}
