b86057d36143b49bc8b01aa6c8c23e90fae4aa3b
Chanwoo Lee <lgw22774@gmail.com>
2025-08-19 22:37:48 +0900

    원소들의곱과합

---
+++ src/proprammers/원소들의곱과합.java
@@ -0,0 +1,19 @@
+package proprammers;
+class Solution14 {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int a = 1;
+        int b = 0;
+        for (int i = 0; i < num_list.length; i++){
+            a *= num_list[i];
+            b += num_list[i];
+        }
+        b = b * b;
+        if ( a > b){
+            answer = 0;
+        }else {
+            answer = 1;
+        }
+        return answer;
+    }
+}
