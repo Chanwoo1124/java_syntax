c0a456560b735412f5c6dd2b728a57f580993d42
Chanwoo1124 <lgw22774@gmail.com>
2025-08-23 15:07:58 +0900

    수조작하기2

---
+++ src/proprammers/수조작하기2.java
@@ -0,0 +1,24 @@
+package proprammers;
+
+class Solution18 {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for (int i = 1; i < numLog.length; i++){
+            if (numLog[i] - numLog[i-1] == 1){
+                answer += "w";
+            }
+            else if (numLog[i] - numLog[i-1] == -1){
+                answer += "s";
+            }
+            else if (numLog[i] - numLog[i-1] == 10){
+                answer += "d";
+            }else{
+                answer += "a";
+            }
+
+
+        }
+
+        return answer;
+    }
+}
