660fd423187ef050b083784978fe9a9f1e46c37e
Chanwoo1124 <lgw22774@gmail.com>
2025-09-03 20:09:24 +0900

    class

--- src/학교수업/methodEx.java
+++ src/학교수업/methodEx.java
@@ -4,8 +4,24 @@ package 학교수업;
 
 //접근 제한자 종류 :public protected default private
 //리턴형 종류 기본자료형 ,객체형, void(리턴형이 없음을 의미)
-
+            //public :누구나 접근해서 사용가능한 메소드
+            //private: 메소드가 정의된 클래스 안에서만 사용 가능
+            //
 import java.sql.SQLOutput;
+
+class  MyCal{
+
+    public int add(int x,int y){
+        int r = x + y;
+        return r;
+    }
+    public double add(double x,double y){
+        double r = x + y;
+        return r;
+    }
+}
+
+
 
 class 라면{
     //접근 제한자, 리턴형 메소드 이름  순으로
@@ -20,5 +36,10 @@ public class methodEx {
     public static void main(String[] args) {
         라면 라면1 = new 라면();
         라면1.라면끓어줘(1000);
+        MyCal a = new MyCal();
+        int r =a.add(1,2);
+        System.out.println(r);
+        double d = a.add(0.1,0.2);
+        System.out.println(d);
     }
 }
--- src/학교수업/성적평균구하기.java
+++ src/학교수업/성적평균구하기.java
@@ -1,17 +1,50 @@
 package 학교수업;
 
-class  MyPersson{
-    private int x1,x2,x3;
-    public void 성적(int i ,int j ,int p){
-//        int result = (i+j+p) /3;
-        System.out.println((i+j+p) /3);
+class  MyPerson{
+    int age;
+    String name;
+    int score1;
+    int score2;
+    int score3;
+    int e = 0; //평균값을 저장하기 위한변수
+
+    public MyPerson(String n,int a, int s1,int s2,int s3){
+        name =n;
+        age = a;
+        score1 = s1;
+        score2 = s2;
+        score3 = s3;
+        e = score1 + score2 + score3;
+    }
+
+
+    public void 성적평균구하기(){
+        int result = score1 + score2 + score3;
+        e = result / 3;
+
+    }
+    public  void 성적표출력(){
+        System.out.println("이름:" + name);
+        System.out.println("나이:" + age);
+        System.out.println("성적평균:" + e);
     }
 }
 
+
 public class 성적평균구하기{
     public static void main(String[] args) {
-        MyPersson m = new MyPersson();
-        m.성적(100,90,80);
-        m.성적(80,80,80);
+    MyPerson m1 = new MyPerson("김길동",20,90,80,70);
+    m1.성적평균구하기();
+    m1.성적표출력();
+    int total = m1.e;
+    MyPerson m2 = new MyPerson("박길동",50,90,80,70);
+    m2.성적평균구하기();
+    m2.성적표출력();
+    total = m2.e;
+    MyPerson m3 = new MyPerson("홍0길동",70,90,80,70);
+    m3.성적평균구하기();
+    m3.성적표출력();
+    total = m3.e;
+
     }
 }
